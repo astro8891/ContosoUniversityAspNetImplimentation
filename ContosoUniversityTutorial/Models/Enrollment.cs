642262cafa68ebd1c000563f16bd1a9178bcb13a
Ashley <Ashley>
2016-04-13 23:15:32 +1200

    init with DB seed and local DB configured

---
+++ ContosoUniversityTutorial/Models/Enrollment.cs
@@ -0,0 +1,19 @@
+﻿namespace ContosoUniversityTutorial.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+
+        //navigation properties
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
